76b1f0ee5457b14a7d60e58bf7caac11f3565e4a
Joliveira <jjoliveirajr@live.com>
2020-10-18 19:12:48 -0200

    inserindo seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(nome: 'primeironome', cpf: '12345678910', agencia: '111', conta: '123', saldo: 500, usuario: 0, password_digest: 'bbb')
+User.create(nome: 'gerente 1', cpf: '11111111111', agencia: '111', conta: '111', saldo: 500, usuario: 0, password_digest: 'bbb')
+User.create(nome: 'gerente 2', cpf: '22222222222', agencia: '222', conta: '222', saldo: 1100, usuario: 0, password_digest: 'bbb')
+User.create(nome: 'correntista 1', cpf: '33333333333', agencia: '333', conta: '333', saldo: 1500, usuario: 1, password_digest: 'bbb')
+User.create(nome: 'correntista 2', cpf: '44444444444', agencia: '444', conta: '444', saldo: 2500, usuario: 1, password_digest: 'bbb')
